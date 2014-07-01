0732fb11f88e074c51a622d28c5074a575ea6314
Alexander Chernikov <alerkesi@gmail.com>
2014-07-01 16:46:45 +0400

    смена основной картинки

--- j.js
+++ j.js
@@ -1,3 +1,34 @@
-var openMap = function(){
-    $('#js-map-popup').show();
-};
+$(document).ready(function () {
+    var openMap = function () {
+        $('#js-map-popup').show();
+    };
+
+    /* change main item images when mouseEnter on little */
+    var $littleImages = $('.js-littlePic');
+    var $mainPic = $('.js-mainPic');
+    var oldSrc = $mainPic.attr('src');
+    var tempChangeMainPic = function (el) {
+        $mainPic.attr('src', $(el).find('img').attr('src'));
+    };
+    var restoreMainPic = function () {
+        $mainPic.attr('src', oldSrc);
+    };
+    var changeMainPic = function (el) {
+        $littleImages.removeClass('selected');
+        $(el).addClass('selected');
+        oldSrc = $(el).find('img').attr('src');
+        $mainPic.attr('src', oldSrc);
+    };
+
+    $littleImages.mouseenter(function () {
+        tempChangeMainPic(this)
+    })
+        .click(function(){
+            changeMainPic(this)
+        });
+    $('.item-images').mouseleave(function () {
+        restoreMainPic();
+    });
+    /* --- */
+
+});
